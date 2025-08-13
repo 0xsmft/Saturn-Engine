ddc8aced8e9407a01847440c2582f2910dd4d00d
BEASTSM96 <beastboioioi.dev@gmail.com>
2025-08-13 21:35:35 +0100

    Begin initial work on Prefab2

---
+++ Saturn/src/Saturn/Asset/Prefab2.h
@@ -0,0 +1,60 @@
+/********************************************************************************************
+*                                                                                           *
+*                                                                                           *
+*                                                                                           *
+* MIT License                                                                               *
+*                                                                                           *
+* Copyright (c) 2020 - 2025 BEAST                                                           *
+*                                                                                           *
+* Permission is hereby granted, free of charge, to any person obtaining a copy              *
+* of this software and associated documentation files (the "Software"), to deal             *
+* in the Software without restriction, including without limitation the rights              *
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                 *
+* copies of the Software, and to permit persons to whom the Software is                     *
+* furnished to do so, subject to the following conditions:                                  *
+*                                                                                           *
+* The above copyright notice and this permission notice shall be included in all            *
+* copies or substantial portions of the Software.                                           *
+*                                                                                           *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                  *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE               *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                    *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,             *
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE             *
+* SOFTWARE.                                                                                 *
+*********************************************************************************************
+*/
+
+#pragma once
+
+#include "Asset.h"
+#include "Saturn/Scene/Entity.h"
+
+namespace Saturn {
+
+	struct PrefabEntity
+	{
+		UUID ID{};
+		UUID ParentID{};
+		uint64_t ClassHash = 0;
+
+		// components...
+	};
+
+	// WIP: New prefab class, will be renamed to Prefab
+	// self-contained prefab
+	class Prefab2 : public Asset
+	{
+	public:
+		Prefab2();
+		Prefab2( const Ref<Asset>& rBase );
+		~Prefab2();
+
+		void CreatePrefabFromSceneEntity( SharedPtr<Entity> entity );
+
+	private:
+		std::map<uint32_t, PrefabEntity> m_Entities;
+	};
+	
+}

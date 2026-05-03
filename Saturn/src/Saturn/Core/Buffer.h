e13058399f6ca65e1ba1c68693a52ffc3e5852ce
0xsmft <beastboioioi.dev@gmail.com>
2026-05-03 15:00:25 +0100

    Add size_t constructor for Buffer
    [no ci]

--- Saturn/src/Saturn/Core/Buffer.h
+++ Saturn/src/Saturn/Core/Buffer.h
@@ -38,6 +38,7 @@ namespace Saturn {
 	{
 	public:
 		Buffer() : Size( 0 ), Data( nullptr ) {}
+		Buffer( size_t size, uint8_t* pData ) : Size( size ), Data( pData ) {}
 		Buffer( uint32_t size, uint8_t* pData ) : Size( size ), Data( pData ) {}
 		Buffer( uint32_t size, void* pData ) : Size( size ), Data( (uint8_t*)pData ) {}
 		~Buffer() {}

99d9645c02b9fa379aa2a068444236d6aead346b
0xsmft <beastboioioi.dev@gmail.com>
2026-07-27 16:05:36 +0100

    Covert SAT_CURRENT_VERSION_BUILD_TAG to macro
    [no ci]

--- Saturn/src/Saturn/Core/Base.h
+++ Saturn/src/Saturn/Core/Base.h
@@ -51,7 +51,8 @@ consteval auto SAT_MAKE_VERSION( Ty major, Ty minor, Ty patch ) { return ( ( ( (
 // Current version is Alpha 0.2.7 (Alpha 2.7)
 constexpr auto SAT_CURRENT_VERSION = SAT_MAKE_VERSION( 0, 2, 7 );
 constexpr auto SAT_CURRENT_VERSION_STRING = "0.2.7";
-constexpr auto SAT_CURRENT_VERSION_BUILD_TAG = "D17KF";
+
+#define SAT_CURRENT_VERSION_BUILD_TAG "D17KF"
 
 #define SAT_DECODE_VERSION(source, major, minor, patch) \
 patch = (source) & 0xFF; \

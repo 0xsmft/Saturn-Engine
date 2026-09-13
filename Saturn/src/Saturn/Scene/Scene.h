ba2c9d7caad7d8d4cd889cdfb9560d766bea5c82
0xsmft <beastboioioi.dev@gmail.com>
2026-09-13 15:50:05 +0100

    Add back GetAllEntitiesWith and GetAllEntitiesWithClass

--- Saturn/src/Saturn/Scene/Scene.h
+++ Saturn/src/Saturn/Scene/Scene.h
@@ -286,13 +286,11 @@ namespace Saturn {
 		std::vector<SharedPtr<Entity>> GetAllEntitiesWith( void )
 		{
 			std::vector<SharedPtr<Entity>> result;
-			/*
 			for( const auto& [ id, entity ] : m_EntityIDMap )
 			{
 				if( entity->SAT_GCC_TEMPLATE HasComponent<T>() )
 					result.push_back( entity );
 			}
-*/
 
 			return result;
 		}
@@ -301,14 +299,12 @@ namespace Saturn {
 		std::vector<SharedPtr<T>> GetAllEntitiesWithClass( void )
 		{
 			std::vector<SharedPtr<T>> result;
-
-			/*
+			
 			for( const auto& [id, entity] : m_EntityIDMap )
 			{
 				if( entity->GetClass() == T::StaticClass() )
 					result.push_back( entity.As<T>() );
 			}
-			*/
 
 			return result;
 		}

188fd862d0325dc5ce589e4b9a667c10252cc9c9
0xsmft <beastboioioi.dev@gmail.com>
2026-09-14 14:50:55 +0100

    Trying to BuildTool to build on macOS....

---
+++ SaturnBuildTool/SBT-ForNonWindows.lua
@@ -0,0 +1,45 @@
+buildtooloutputdir = "%{cfg.buildcfg}-%{cfg.system}-AnyCPU"
+
+include (path.join( "Saturn-Editor", "content", "Templates", "PremakeCSExtensions.lua"))
+
+workspace "SaturnBuildTool"
+	startprojet "SaturnBuildTool"
+	warnings "Default"
+	configurations { "Debug-ASan", "Debug", "Release", "Dist" }
+
+project "SaturnBuildTool"
+	language "C#"
+	kind "ConsoleApp"
+	dotnetframework "net9.0"
+
+	targetdir ("../bin/" .. buildtooloutputdir .. "/%{prj.name}")
+	objdir ("../bin-int/" .. buildtooloutputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.cs"
+	}
+
+	postbuildcommands
+	{
+		'{COPY} "../../../SaturnBuildTool/RT" "RT/"'
+	}
+
+	propertytags {
+        { "AppendTargetFrameworkToOutputPath", "false" },
+        { "Version", "5.1.0" },
+        { "Company", "Saturn" }
+	}
+
+	nuget { "Microsoft.CodeAnalysis.CSharp:5.9.0" }
+
+	filter { "configurations:Debug" }
+		symbols "On"
+
+ 	filter { "configurations:Release" }
+		optimize "On"
+   		symbols "On"
+
+	filter { "configurations:Dist" }
+		optimize "On"
+  		symbols "Off"

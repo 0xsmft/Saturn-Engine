77be6ec9b29878e4841cee4c867d7147c48e935e
0xsmft <beastboioioi.dev@gmail.com>
2026-09-16 09:36:42 +0100

    Update macOS game premake project files to generate correct files

--- Saturn-Editor/content/Templates/premake5.lua
+++ Saturn-Editor/content/Templates/premake5.lua
@@ -2,6 +2,7 @@
 -- __PROJECT_NAME__
 
 workspace "__PROJECT_NAME__"
+	-- This doesnt effect the output binary.
 	architecture "x64"
 	startproject "__PROJECT_NAME__"
 
@@ -29,15 +30,16 @@ project "Saturn"
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	debugcommand ( "" )
-	buildcommands   ( "@rem nothing to build" )
-	rebuildcommands ( "@rem nothing to build" )
-	cleancommands   ( "@rem nothing to build" )
+	buildcommands   ( "" )
+	rebuildcommands ( "" )
+	cleancommands   ( "" )
 
 	files 
 	{
 		SaturnDir .. "/Saturn/src/**.cpp",
 		SaturnDir .. "/Saturn/src/**.h",
 		SaturnDir .. "/Saturn/src/**.cs",
+		SaturnDir .. "/Saturn/src/**.mm",
 	}
 
 -- __PROJECT_NAME__
@@ -76,20 +78,23 @@ project "__PROJECT_NAME__"
 			debugargs { "%{wks.location}/%{prj.name}.sproject" }
 			debugdir ( SaturnDir .. "/Saturn-Editor" )
 
-			buildcommands
-			{
-				SaturnDir .. "/bin/Debug-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /BUILD /NAME:%{prj.name} /Win64 /Debug /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Debug" }
+				buildcommands
+				{
+					SaturnDir .. "/bin/Debug-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /BUILD /NAME:%{prj.name} /Win64 /Debug /PROJECT:%{wks.location}"
+				}
 
-			rebuildcommands 
-			{
-				SaturnDir .. "/bin/Debug-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /REBUILD /NAME:%{prj.name} /Win64 /Debug /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Debug" }
+				rebuildcommands 
+				{
+					SaturnDir .. "/bin/Debug-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /REBUILD /NAME:%{prj.name} /Win64 /Debug /PROJECT:%{wks.location}"
+				}
 
-			cleancommands
-			{
-				SaturnDir .. "/bin/Debug-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /CLEAN /NAME:%{prj.name} /Win64 /Debug /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Debug" }
+				cleancommands
+				{
+					SaturnDir .. "/bin/Debug-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /CLEAN /NAME:%{prj.name} /Win64 /Debug /PROJECT:%{wks.location}"
+				}
 
 		filter "configurations:Release"
 			runtime "Release"
@@ -99,36 +104,118 @@ project "__PROJECT_NAME__"
 			debugargs    { "%{wks.location}/%{prj.name}.sproject" }
 			debugdir     ( SaturnDir .. "/Saturn-Editor" )
 
-			buildcommands
-			{
-				SaturnDir .. "/bin/Release-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /BUILD /NAME:%{prj.name} /Win64 /Release /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Release" }
+				buildcommands
+				{
+					SaturnDir .. "/bin/Release-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /BUILD /NAME:%{prj.name} /Win64 /Release /PROJECT:%{wks.location}"
+				}
 
-			rebuildcommands 
-			{
-				SaturnDir .. "/bin/Release-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /REBUILD /NAME:%{prj.name} /Win64 /Release /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Release" }
+				rebuildcommands 
+				{
+					SaturnDir .. "/bin/Release-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /REBUILD /NAME:%{prj.name} /Win64 /Release /PROJECT:%{wks.location}"
+				}
 
-			cleancommands
-			{
-				SaturnDir .. "/bin/Release-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /CLEAN /NAME:%{prj.name} /Win64 /Release /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Release" }
+				cleancommands
+				{
+					SaturnDir .. "/bin/Release-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /CLEAN /NAME:%{prj.name} /Win64 /Release /PROJECT:%{wks.location}"
+				}
 
 		filter "configurations:Dist"
 			runtime "Release"
 			symbols "on"
 
-			buildcommands
-			{
-				SaturnDir .. "/bin/Dist-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /BUILD /NAME:%{prj.name} /Win64 /Dist /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Dist" }
+				buildcommands
+				{
+					SaturnDir .. "/bin/Dist-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /BUILD /NAME:%{prj.name} /Win64 /Dist /PROJECT:%{wks.location}"
+				}
 
-			rebuildcommands 
-			{
-				SaturnDir .. "/bin/Dist-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /REBUILD /NAME:%{prj.name} /Win64 /Dist /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Dist" }
+				rebuildcommands 
+				{
+					SaturnDir .. "/bin/Dist-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /REBUILD /NAME:%{prj.name} /Win64 /Dist /PROJECT:%{wks.location}"
+				}
 
-			cleancommands
-			{
-				SaturnDir .. "/bin/Dist-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /CLEAN /NAME:%{prj.name} /Win64 /Dist /PROJECT:%{wks.location}"
-			}
+			filter { "system:windows", "configurations:Dist" }
+				cleancommands
+				{
+					SaturnDir .. "/bin/Dist-windows-x86_64/SaturnBuildTool/SaturnBuildTool.exe /CLEAN /NAME:%{prj.name} /Win64 /Dist /PROJECT:%{wks.location}"
+				}
+
+	filter "system:macosx"
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+
+			debugcommand ( SaturnDir .. "/bin/Debug-macosx-AARCH64/Saturn-Editor/Saturn-Editor" )
+			debugargs { "%{wks.location}/%{prj.name}.sproject" }
+			debugdir ( SaturnDir .. "/Saturn-Editor" )
+
+			filter { "system:macosx", "configurations:Debug" }
+				buildcommands
+				{
+					-- This is so bad, so fucking bad, but premake gives us the relative path and not the abs path
+					SaturnDir .. "/bin/Debug-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /BUILD /NAME:%{prj.name} /Apple /Debug /PROJECT:" .. path.getabsolute(".")
+				}
+
+			filter { "system:macosx", "configurations:Debug" }
+				rebuildcommands 
+				{
+					SaturnDir .. "/bin/Debug-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /REBUILD /NAME:%{prj.name} /Apple /Debug /PROJECT:" .. path.getabsolute(".")
+				}
+
+			filter { "system:macosx", "configurations:Debug" }
+				cleancommands
+				{
+					SaturnDir .. "/bin/Debug-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /CLEAN /NAME:%{prj.name} /Apple /Debug /PROJECT:" .. path.getabsolute(".")
+				}
+
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "on"
+
+			debugcommand ( SaturnDir .. "/bin/Release-macosx-AARCH64/Saturn-Editor/Saturn-Editor" )
+			debugargs    { "%{wks.location}/%{prj.name}.sproject" }
+			debugdir     ( SaturnDir .. "/Saturn-Editor" )
+
+			filter { "system:macosx", "configurations:Release" }
+				buildcommands
+				{
+					SaturnDir .. "/bin/Release-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /BUILD /NAME:%{prj.name} /Apple /Release /PROJECT:%{wks.location}"
+				}
+
+			filter { "system:macosx", "configurations:Release" }
+				rebuildcommands 
+				{
+					SaturnDir .. "/bin/Release-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /REBUILD /NAME:%{prj.name} /Apple /Release /PROJECT:%{wks.location}"
+				}
+
+			filter { "system:macosx", "configurations:Release" }
+				cleancommands
+				{
+					SaturnDir .. "/bin/Release-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /CLEAN /NAME:%{prj.name} /Apple /Release /PROJECT:%{wks.location}"
+				}
+
+		filter "configurations:Dist"
+			runtime "Release"
+			symbols "on"
+
+			filter { "system:macosx", "configurations:Dist" }
+				buildcommands
+				{
+					SaturnDir .. "/bin/Dist-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /BUILD /NAME:%{prj.name} /Apple /Dist /PROJECT:%{wks.location}"
+				}
+
+			filter { "system:macosx", "configurations:Dist" }
+				rebuildcommands 
+				{
+					SaturnDir .. "/bin/Dist-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /REBUILD /NAME:%{prj.name} /Apple /Dist /PROJECT:%{wks.location}"
+				}
+
+			filter { "system:macosx", "configurations:Dist" }
+				cleancommands
+				{
+					SaturnDir .. "/bin/Dist-macosx-AnyCPU/SaturnBuildTool/RT/Run.sh /CLEAN /NAME:%{prj.name} /Apple /Dist /PROJECT:%{wks.location}"
+				}

cdb96a5271abeb7a71d2c24b7886707bc67f10d4
0xsmft <beastboioioi.dev@gmail.com>
2026-09-19 15:41:56 +0100

    Ruby,Cocoa: Add native menu bar

--- Saturn/src/Saturn/Core/Ruby/Backend/RubyNSApp.mm
+++ Saturn/src/Saturn/Core/Ruby/Backend/RubyNSApp.mm
@@ -35,6 +35,34 @@
 #include "Saturn/Core/App.h"
 #include "Saturn/Core/Ruby/RubyWindow.h"
 
+static void InitMenuBar()
+{
+	NSMenu* pBar = [[NSMenu alloc] init];
+	[NSApp setMainMenu:pBar];
+
+	// App menu item.
+	NSMenuItem* pAppMenuItem = [pBar addItemWithTitle:@"" action:NULL keyEquivalent:@""];
+	NSMenu* pAppMenu = [[NSMenu alloc] init];
+	[pAppMenuItem setSubmenu:pAppMenu];
+
+	// About.
+	[pAppMenu addItemWithTitle:@"About Saturn"
+				action:@selector(orderFrontStandardAboutPanel:)
+				keyEquivalent:@""];
+	
+	// Separator.
+	[pAppMenu addItem:[NSMenuItem separatorItem]];
+
+	// Quit.
+	[pAppMenu addItemWithTitle:@"Quit Saturn"
+				action:@selector(terminate:)
+				keyEquivalent:@"q"];
+
+	// Black magic.
+	SEL setAppleMenuSelector = NSSelectorFromString( @"setAppleMenu" );
+	[NSApp performSelector:setAppleMenuSelector withObject:pAppMenu];
+}
+
 @interface RubyNSApplicationDelegate : NSObject <NSApplicationDelegate>
 @end
 
@@ -54,9 +82,14 @@ namespace Saturn {
 	[NSApp stop:nil];
 }
 
+- (void)applicationWillFinishLaunching:(NSNotification *)notification
+{
+	InitMenuBar();
+}
+
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
 {
-	Saturn::Application::Get()->GetWindow()->FlashAttention();
+	Saturn::Application::Get()->Close();
 	return NSTerminateCancel;
 }
 
